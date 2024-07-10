8c28f7a87b870ac80a170c8c3a8300181dba3391
Alabi Tosin <alabitosin60@gmail.com>
2024-07-10 22:11:21 +0100

    updated the auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,24 +3,46 @@ require('dotenv').config();
 
 const {SECRET} = process.env;
 
-const authentication = (req, res, next) => {
-    try {
-      const header = req.headers.token || req.body.token || req.headers.authorization;
-      if (!header || header === '') return res.status(401).json({ 
-        status: 'Bad request', 
-        message: 'Authentication failed',
-        statusCode: 401 
-      });
-      const token = jwt.verify(header, SECRET);
-      req.decode = token;
-      next();
-    } catch (e) {
+// const authentication = (req, res, next) => {
+//     try {
+//       const header = req.headers.token || req.body.token || req.headers.authorization;
+//       if (!header || header === '') return res.status(401).json({ 
+//         status: 'Bad request', 
+//         message: 'Authentication failed',
+//         statusCode: 401 
+//       });
+//       const token = jwt.verify(header, SECRET);
+//       req.decode = token;
+//       next();
+//     } catch (e) {
+//       return res.status(401).json({ 
+//         status: 'Bad request', 
+//         message: 'Invalid token',
+//         statusCode: 401 
+//       });
+//     }
+//   };
+
+  const authentication = async (req, res, next) => {
+    if (!req.headers.authorization) {
       return res.status(401).json({ 
-        status: 'Bad request', 
-        message: 'Invalid token',
-        statusCode: 401 
-      });
+          status: 'Bad request', 
+          message: 'Authentication failed',
+          statusCode: 401 
+        });
     }
-  };
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+      const decoded = await jwt.verify(token, SECRET);
+      req.decode = decoded;
+      return next();
+    } catch (err) {
+      return res.status(401).json({ 
+          status: 'Bad request', 
+          message: 'Authentication failed',
+          statusCode: 401 
+        });
+    }
+  }
 
 module.exports = authentication;
