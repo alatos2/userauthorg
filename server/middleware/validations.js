201fa2eed2cbe1afb2da83b89183490508266b23
Alabi Tosin <alabitosin60@gmail.com>
2024-07-10 22:43:35 +0100

    updated validation.js and tables.js

--- server/middleware/validations.js
+++ server/middleware/validations.js
@@ -4,8 +4,8 @@ const validateCreateUser = [
     body('firstName', 'First name is required').notEmpty(),
     body('lastName', 'Last name is required').notEmpty(),
     body('email', 'Email is required or not in the right format').notEmpty().isEmail(),
-    body('password', 'Password is required').notEmpty(),
-    body('phone', 'Phone is required').notEmpty()
+    body('password', 'Password is required').notEmpty()
+    // body('phone', 'Phone is required').notEmpty()
 ]
 
 const validateLoginUser = [
--- server/models/tables.js
+++ server/models/tables.js
@@ -13,7 +13,7 @@ const createTables = () => {
             lastName VARCHAR(128) NOT NULL,     
             email VARCHAR(128) UNIQUE NOT NULL,
             password VARCHAR(128) NOT NULL,
-            phone VARCHAR(128) NOT NULL
+            phone VARCHAR(128) NULL
         )
     `;
 
